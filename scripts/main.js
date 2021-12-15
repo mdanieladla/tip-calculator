8a1e4dd9139f9bed7c45860b48633b73cbc623fd
mdanieladla <mariadanieladarnea@gmail.com>
2021-12-15 12:11:04 +0100

    added resetbtn, validation input people and total preview

--- scripts/main.js
+++ scripts/main.js
@@ -4,24 +4,55 @@ const inputBill = document.querySelector('.js-input-bill');
 const customTip = document.querySelector('.js-custom');
 const inputNumberPeople = document.querySelector('.js-number-people');
 const btnTip = document.querySelectorAll('.js-tip-btn');
+const total = document.querySelector('.js-total');
+const errNPeople = document.querySelector('.err-message');
+const resetBtn = document.querySelector('.js-reset-btn');
 
-//funcion para coger el valor del input de la cuenta.
+/* **************funcion para calcular el total por persona************** */
 function handleInputBill() {
   let inputValue = inputBill.value;
-  //   console.log(inputValue);
+  let inputPeopleValue = inputNumberPeople.value;
+  let totalF = inputValue / inputPeopleValue;
+  total.innerHTML = '$' + totalF.toFixed(2);
 }
 
-inputBill.addEventListener('keyup', handleInputBill);
+inputNumberPeople.addEventListener('keyup', handleInputBill);
+
+/* ******funcion para que se ponga en el total/persona el total de la cuenta nada mas introducir el valor en el input bill.******* */
+function testBill() {
+  let inputValue = inputBill.value;
+  total.innerHTML = `$ ${inputValue}`;
+  validateNPeople();
+}
+
+inputBill.addEventListener('keyup', testBill);
+
+/* **************funcion para validar number of people************** */
+const validateNPeople = () => {
+  let valueInputPeople = inputNumberPeople.value;
+  if (valueInputPeople == 0) {
+    return (errNPeople.innerHTML = "Can't be zero");
+  } else {
+    errNPeople.innerHTML = '';
+  }
+};
 
 //funcion para coger el valor del input del numero de personas
-function handleInputPeople() {
-  let inputPeopleValue = inputNumberPeople.value;
-  //   console.log(inputPeopleValue);
-}
+// function handleInputPeople() {
+//   let inputPeopleValue = inputNumberPeople.value;
+//   //   console.log(inputPeopleValue);
+// }
 
-inputNumberPeople.addEventListener('keyup', handleInputPeople);
+// inputNumberPeople.addEventListener('keyup', handleInputPeople);
 
-//funcion para coger el valor del input del % tip
+/* ***********funcion para que al clickar sobre cada boton de la propina le cambie el estilo ************** */
+btnTip.forEach((btn) =>
+  btn.addEventListener('click', () => {
+    btn.classList.toggle('active');
+  })
+);
+
+/* **************funcion para coger el valor del input del % tip************** */
 function handleCustomTip() {
   let inputCustom = customTip.value;
   //   console.log(inputCustom);
@@ -29,9 +60,9 @@ function handleCustomTip() {
 
 customTip.addEventListener('keyup', handleCustomTip);
 
-//funcion para que al clickar sobre cada boton de la propina le cambie el estilo
-btnTip.forEach((btn) =>
-  btn.addEventListener('click', () => {
-    btn.classList.toggle('active');
-  })
-);
+/* **************reset boton************** */
+function handleClickResetBtn() {
+  location.reload();
+}
+
+resetBtn.addEventListener('click', handleClickResetBtn);

dd5c6f713d290163a19888ba41b578d08ae83d88
mdanieladla <mariadanieladarnea@gmail.com>
2021-12-20 17:52:21 +0100

    delete some useless functions and tried tip thing but not working

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@ const btnTip = document.querySelectorAll('.js-tip-btn');
 const total = document.querySelector('.js-total');
 const errNPeople = document.querySelector('.err-message');
 const resetBtn = document.querySelector('.js-reset-btn');
+const tipAmount = document.querySelector('.js-tip-amount');
 
 /* **************funcion para calcular el total por persona************** */
 function handleInputBill() {
@@ -18,6 +19,21 @@ function handleInputBill() {
 
 inputNumberPeople.addEventListener('keyup', handleInputBill);
 
+//no termina de funcionar :(((((((((((((
+// function handleTips() {
+//   btnTip.forEach((btn) =>
+//     btn.addEventListener('click', () => {
+//       let value = btn.value;
+//       // console.log(value);
+//       let inputValue = inputBill.value;
+//       let inputPeopleValue = inputNumberPeople.value;
+//       let total = (inputValue * value) / 100 / inputPeopleValue;
+//       tipAmount.innerHTML = '$' + total.toFixed(2);
+//     })
+//   );
+// }
+// inputNumberPeople.addEventListener('keyup', handleTips);
+
 /* ******funcion para que se ponga en el total/persona el total de la cuenta nada mas introducir el valor en el input bill.******* */
 function testBill() {
   let inputValue = Number(inputBill.value);
@@ -27,30 +43,13 @@ function testBill() {
 
 inputBill.addEventListener('keyup', testBill);
 
-/* **************funcion para validar number of people************** */
-const validateNPeople = () => {
-  let valueInputPeople = inputNumberPeople.value;
-  if (valueInputPeople <= 0) {
-    return (errNPeople.innerHTML = "Can't be zero");
-  }
-};
-//funcion para coger el valor del input del numero de personas
-// function handleInputPeople() {
-//   let inputPeopleValue = inputNumberPeople.value;
-//   //   console.log(inputPeopleValue);
-// }
-
-// inputNumberPeople.addEventListener('keyup', handleInputPeople);
-
-console.log(btnTip.value);
-
 /* ***********funcion para que al clickar sobre cada boton de la propina le cambie el estilo ************** */
 btnTip.forEach((btn) =>
   btn.addEventListener('click', () => {
     btn.classList.toggle('active');
   })
 );
-
+/* ***********funcion para que al clickar sobre cada boton de la propina coja el valor de cada uno ************** */
 btnTip.forEach((btn) =>
   btn.addEventListener('click', () => {
     let getValue = btn.value;
